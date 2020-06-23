6a9de943228539d2f6c398979c4e37922e1e283b
tianyacike2012 <tianyacike2012@163.com>
2020-06-23 15:36:52 +0800

    新建boot4项目

---
+++ src/main/java/com/kl/Boot4Application.java
@@ -0,0 +1,13 @@
+package com.kl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot4Application.class, args);
+	}
+
+}
