ca5ed852b4a1edf8c846094c62e9e19aee9d6110
tianyacike2012 <tianyacike2012@163.com>
2020-06-23 15:52:28 +0800

    新增HelloController

---
+++ src/main/java/com/kl/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.kl.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/info")
+    public String info(){
+        return "info";
+    }
+}
